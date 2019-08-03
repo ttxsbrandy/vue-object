32502fa69a01de06c27c5ae3e25327472d1fc680
why <1005580669@qq.com>
2019-08-03 10:05:51 +0800

    test why

--- jingpai/src/App.vue
+++ jingpai/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home test！</router-link> |
+      <router-link to="/">Home  是个大数据的开发哈市大河网test！</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
