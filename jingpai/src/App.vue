2b0628daded720d17dfea8686c7970f2f3c0a5a3
why <1005580669@qq.com>
2019-08-03 10:42:49 +0800

    bb

--- jingpai/src/App.vue
+++ jingpai/src/App.vue
@@ -1,13 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-
       <router-link to="/">Home</router-link> |123123
-4d56e70a2decff49d1bf87d1d78a4298f6bccfc5
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
   </div>
 </template>
-
-
